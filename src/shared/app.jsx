28adb0ff7895dc515501a53fe9256e644f08e962
Nick Nelson <ntylernelson@gmail.com>
2017-03-16 14:22:10 -0700

    Added JSS && Bootstrap styling

--- src/shared/app.jsx
+++ src/shared/app.jsx
@@ -18,7 +18,7 @@ import {
 } from './routes'
 
 const App = () =>
-  <div>
+  <div style={{ paddingTop: 54 }}>
     <Helmet titleTemplate={`%s | ${APP_NAME}`} defaultTitle={APP_NAME} />
     <h1>{APP_NAME}</h1>
     <Nav />
