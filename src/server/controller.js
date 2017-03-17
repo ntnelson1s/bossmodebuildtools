41af81954ecc80c5914abf7da9d0b96e786e66f3
Nick Nelson <ntylernelson@gmail.com>
2017-03-17 00:51:11 -0700

    Checking Heroku Prod pushing

--- src/server/controller.js
+++ src/server/controller.js
@@ -3,11 +3,11 @@
 export const homePage = () => null
 
 export const helloPage = () => ({
-  hello: { message: 'Server-side preloaded message' },
+  hello: { message: 'Server-side preloaded message.' },
 })
 
 export const helloAsyncPage = () => ({
-  hello: { messageAsync: 'Server-side preloaded message for async page' },
+  hello: { messageAsync: 'Server-side preloaded message for async page.' },
 })
 
 export const helloEndpoint = (num: number) => ({
